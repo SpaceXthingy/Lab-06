87961483aff24eb5e2e4c6199ec13a380f83371b
tkare <timothy12336@gmail.com>
2023-10-30 19:04:38 -0400

    I implemented the decode function and made a slight change on your encode function.

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ def encode():
     new_user_password = ""
     for i in range(len(user_password)):
         new_user_password = new_user_password + (str(int(user_password[i]) + 3))[-1]
-    print(new_user_password)
+    return (new_user_password)
 
-encode()
+def decode(encoded_password):
+    new_user_password = ""
+    for i in range(len(encoded_password)):
+        new_user_password = new_user_password + (str(int(encoded_password[i]) - 3))[-1]
 
+    return new_user_password
 
-#def decode():
 
 
